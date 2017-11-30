30ad96843e0c92c3adf207cfdb38e25a1dfc600b
chenzhang <630457477@qq.com>
2017-11-30 17:43:26 +0800

    增加之前缺失的字体; 修改主页的图片大小问题; 修改登录页面验证文字是英文的问题; 增加了一级菜单中'对账''电商管理'的结构和对应的二级目录; 修改alertMsg的样式;

--- public/static/admin/js/en-us.js
+++ public/static/admin/js/en-us.js
@@ -19,7 +19,8 @@ function zh_validator() {
     }, "Input error");
 
     $.extend($.validator.messages, {
-        required: "This field is required.",
+        //required: "This field is required.",
+        required: "此处不能为空",
         remote: "Please fix this field.",
         email: "Please enter a valid email address.",
         url: "Please enter a valid URL.",
@@ -30,7 +31,8 @@ function zh_validator() {
         equalTo: "Please enter the same value again.",
         maxlength: $.validator.format( "Please enter no more than {0} characters." ),
         minlength: $.validator.format( "Please enter at least {0} characters." ),
-        rangelength: $.validator.format( "Please enter a value between {0} and {1} characters long." ),
+        //rangelength: $.validator.format( "Please enter a value between {0} and {1} characters long." ),
+        rangelength: $.validator.format( "输入文本长度请控制在{0}到{1}之间" ),
         range: $.validator.format( "Please enter a value between {0} and {1}." ),
         max: $.validator.format( "Please enter a value less than or equal to {0}." ),
         min: $.validator.format( "Please enter a value greater than or equal to {0}." ),
