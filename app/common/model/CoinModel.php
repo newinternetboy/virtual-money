c0d49881194d9ca5294f3781972cc613846782cb
internetgod <961937794@qq.com>
2018-04-25 23:31:56 +0800

    虚拟币管理

---
+++ app/common/model/CoinModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/25 0025
+ * Time: 21:48
+ */
+
+namespace app\common\model;
+
+
+class CoinModel extends Common
+{
+    public $table ="coin";
+}
