4cb1d2ada390daae57fbbf27c50f8408c5f3be7c
internetgod <961937794@qq.com>
2018-04-30 23:49:45 +0800

    发币脚本/外加相应配置文件修改/到时候windows脚本我会根据实际环境提供

---
+++ app/timetask/model/Release.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/30 0030
+ * Time: 23:35
+ */
+
+namespace app\timetask\model;
+
+use think\Model;
+class Release extends Model
+{
+    protected $table = 'release';
+}
