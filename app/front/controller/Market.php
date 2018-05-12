5cd4290fe4fec3654ccefe5caeb8e75cd9ef8d87
internetgod <961937794@qq.com>
2018-05-12 22:47:54 +0800

    行情所有功能/包括实时汇率

--- app/front/controller/Market.php
+++ app/front/controller/Market.php
@@ -7,11 +7,35 @@
  */
 
 namespace app\front\controller;
-
+use think\Db;
 class Market extends Home
 {
     public function index(){
-        return $this->fetch();
+        $result = Db::query("select c.name,cl.price_now,cl.rate from coinpricelog cl
+        left join coin c on cl.coin_id = c.id
+        order by cl.create_time desc limit 1");
+        //1人民币兑美元
+        $rb_us = $this->convertCurrency();
+        if ($result){
+            foreach ($result as $k=>$v){
+                $result[$k]['us_price'] = round($v['price_now']*$rb_us,2);
+                $result[$k]['price_now'] = round($v['price_now'],2);
+            }
+        }
+        return $this->fetch('index',['result'=>$result]);
     }
 
+
+    //1人民币兑美元数
+    public 	function convertCurrency(){
+        $from ='CNY';
+        $to = "USD";
+        $data = file_get_contents("http://www.baidu.com/s?wd={$from}%20{$to}");
+        preg_match("/<div>1\D*=(\d*\.\d*)\D*<\/div>/",$data, $converted);
+
+        $converted = preg_replace("/[^0-9.]/", "", $converted[1]);
+
+        $result = round($converted, 4);
+        return $result;
+    }
 }
