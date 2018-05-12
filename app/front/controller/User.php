0b4368546fd136632cb6fd5b17d3abc7fd2b3dab
duxiaoran <duxiaoran123@163.com>
2018-05-12 19:50:50 +0800

    新加入几个页面

--- app/front/controller/User.php
+++ app/front/controller/User.php
@@ -14,4 +14,16 @@ class User extends Home
         return $this->fetch();
     }
 
+    public function login(){
+        return $this->fetch();
+    }
+
+    public function register(){
+        return $this->fetch();
+    }
+
+    public function userAgreement(){
+        return $this->fetch('useragreement');
+    }
+
 }
